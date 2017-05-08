74a21d43eb7a2e6916215c8785d35805dfbf25a7
Tyler Small <tyler.small@spr.com>
2017-05-08 13:41:24 -0500

    Add IssuesControllerTest to project

---
+++ LobbyingMadeSimple.Tests/Controllers/IssuesControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LobbyingMadeSimple.Interfaces;
+
+namespace LobbyingMadeSimple.Tests.Controllers
+{
+    [TestClass]
+    public class IssuesControllerTest
+    {
+        private IIssueRepository _repo;
+        [TestInitialize]
+        public void TestInit()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
