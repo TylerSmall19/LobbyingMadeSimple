03371388dcefe52499c1d6e2607038abef22668e
Tyler Small <intrasolarmusic@gmail.com>
2017-05-19 14:29:46 -0500

    Js currency formatter (General Use Currency formats) (#62)
    * Add currency.js to project; Include it in create.cshtml
    
    * Add selector to currency input field for JS usage
    
    * Format currency on field changes, including copy/paste (in modern browsers)
    
    * Add CSS class to IssuesCreate form; refactor to use hard-coded controller and action names
    
    * Add logic to strip commas from currency fields to get rid of the errors from form submission

---
+++ LobbyingMadeSimple/Scripts/currency.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+    var $currencyFields = $('.currency-field');
+
+    // Add commas when things first get loaded in
+    addCommas($currencyFields);
+    // Handle subsequent changes to the currency input fields
+    $currencyFields.on('input', addCommas.bind(this, $currencyFields));
+    $('.currency-form').on('submit', stripCurrencyFieldCommasBeforeSubmission);
+});
+
+function addCommas(fields) {
+    fields.each(function (i, field) {
+        // Strip commas to get around parseInt issues
+        var numberString = stripCommas(field.value);
+        var newVal = formatNumberToCurrency(numberString);
+
+        field.value = newVal == 'NaN' ? 0 : newVal;
+    });
+}
+
+function stripCommas(string) {
+    return string.replace(/,/g, '');
+}
+
+function formatNumberToCurrency(numberString) {
+    return new Intl.NumberFormat().format(parseInt(numberString, 10));
+}
+
+function stripCurrencyFieldCommasBeforeSubmission(e) {
+    $(this)
+        .find('.currency-field')
+        .each(function (i, field) {
+            field.value = field.value.replace(/,/g, '');
+        });
+}
