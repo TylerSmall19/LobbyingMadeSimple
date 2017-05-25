b60429b1800202ccc84bbbe805ceaeb13bc3d001
Tyler Small <intrasolarmusic@gmail.com>
2017-05-24 20:47:01 -0500

    Add DetailsViewModelTest to project with failing TODO test (#78)

---
+++ LobbyingMadeSimple.Tests/Models/DetailsViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LobbyingMadeSimple.Tests.Models
+{
+    [TestClass]
+    public class DetailsViewModelTest
+    {
+        [TestMethod]
+        public void Has_Tests()
+        {
+            Assert.Fail("TODO: Fix Tests");
+        }
+    }
+}
