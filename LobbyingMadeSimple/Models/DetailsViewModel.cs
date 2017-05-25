aa36a00e9c76692ad88a66a634119834bb779695
Tyler Small <tyler.small@spr.com>
2017-05-25 11:12:03 -0500

    Add generate properties in DetailsViewModel

--- LobbyingMadeSimple/Models/DetailsViewModel.cs
+++ LobbyingMadeSimple/Models/DetailsViewModel.cs
@@ -8,5 +8,15 @@ namespace LobbyingMadeSimple.Web.Models
     public class DetailsViewModel
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public int VoteCount { get; set; }
+        public string IssueScope { get; set; }
+        public string ApprovalPercentage { get; set; }
+        public string FundingRaised { get; set; }
+        public string FundingGoal { get; set; }
+        public string UpvoteButtonColor { get; set; }
+        public string DownvoteButtonColor { get; set; }
     }
 }
