0d4fd78257ea4a17217816e43017d807867c4e40
Tyler Small <tyler.small@spr.com>
2017-05-25 11:05:59 -0500

    Add DetailsViewModel to project

---
+++ LobbyingMadeSimple/Models/DetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LobbyingMadeSimple.Web.Models
+{
+    public class DetailsViewModel
+    {
+        public int Id { get; set; }
+    }
+}
