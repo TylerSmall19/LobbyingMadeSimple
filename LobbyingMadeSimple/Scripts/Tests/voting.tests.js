01c8dc6df23e5a88273b234ccde7fa300827d68e
Tyler Small <intrasolarmusic@gmail.com>
2017-05-30 13:54:53 -0500

    Voting js tests (#93)
    * Add voting.tests.js to the project; include it in the test runner
    
    * Write a test for the failure callback in voting.js; Tests that a div wth matching ID is removed
    
    * Write a test for inverse conditions of first test; Div is left in the DOM if issue is votable
    
    * Fix minor semi-colon issue; Write test to ensure only the proper ID is being removed from the DOM.
    
    * Write a test to ensure if no ID is passed nothing is removed
    
    * Write a test to verify edge case behavior when null is passed
    
    * Update test to pass in issueId on votable-issue condition

---
+++ LobbyingMadeSimple/Scripts/Tests/voting.tests.js
@@ -0,0 +1,95 @@
+﻿QUnit.module("Voting", function () {
+
+	QUnit.module("voteFailed()", function () {
+		QUnit.test("Removes the element with a matching ID when not votable", function (assert) {
+			// Arrange
+            var fixture = $("#qunit-fixture");
+            fixture.append("<div id='1'>hello!</div>");
+            var xhr = { responseJSON: { issueId: 1, isVotable: false } };
+
+            // Verify DIV was added
+            assert.equal($("div", fixture).length, 1, "div added successfully!");
+
+            // Act
+            voteFailed(xhr);
+
+            //Assert
+            assert.equal($("div", fixture).length, 0, "div removed");
+        });
+        
+        QUnit.test("Leaves the div available if votable", function (assert) {
+            // Arrange
+            var fixture = $("#qunit-fixture");
+            fixture.append("<div id='1'></div>");
+            var xhr = { responseJSON: { issueId: 1, isVotable: true } };
+
+            // Verify DIV was added
+            assert.equal($("div", fixture).length, 1, "div added successfully!");
+
+            // Act
+            voteFailed(xhr);
+
+            //Assert
+            assert.equal($("div", fixture).length, 1, "div not removed");
+        });
+
+        QUnit.test("Removes only the div with the given ID", function (assert) {
+            // Arrange
+            var fixture = $("#qunit-fixture");
+            fixture.append("<div id='1'></div>");
+            fixture.append("<div id='2'></div>");
+            fixture.append("<div id='3'></div>");
+            fixture.append("<div id='4'></div>");
+            // Verify DIVs were added
+            assert.equal($("div", fixture).length, 4, "divs added successfully!");
+
+            var xhr = { responseJSON: { issueId: 3, isVotable: false } };
+
+            // Act
+            voteFailed(xhr);
+
+            //Assert
+            assert.equal($("div", fixture).length, 3, "div not removed");
+
+            assert.equal($("div", fixture)[0].id, 1);
+            assert.equal($("div", fixture)[1].id, 2);
+            assert.equal($("div", fixture)[2].id, 4);
+        });
+
+        QUnit.test("Leaves all DIVs in the DOM if no ID is passed", function (assert) {
+            // Arrange
+            var fixture = $("#qunit-fixture");
+            fixture.append("<div id='1'>hello!</div>");
+            fixture.append("<div id='2'>hello!</div>");
+            fixture.append("<div id='3'>hello!</div>");
+            var xhr = { responseJSON: { isVotable: false } };
+
+            // Verify DIV was added
+            assert.equal($("div", fixture).length, 3, "div added successfully!");
+
+            // Act
+            voteFailed(xhr);
+
+            //Assert
+            assert.equal($("div", fixture).length, 3, "no divs removed");
+        });
+
+        QUnit.test("Leaves all DIVs in the DOM if no params are passed", function (assert) {
+            // Arrange
+            var fixture = $("#qunit-fixture");
+            fixture.append("<div id='1'>hello!</div>");
+            fixture.append("<div id='2'>hello!</div>");
+            fixture.append("<div id='3'>hello!</div>");
+            var xhr = { responseJSON: { issueId: null, isVotable: null } };
+
+            // Verify DIV was added
+            assert.equal($("div", fixture).length, 3, "div added successfully!");
+
+            // Act
+            voteFailed(xhr);
+
+            //Assert
+            assert.equal($("div", fixture).length, 3, "no divs removed");
+        });
+	});
+});
