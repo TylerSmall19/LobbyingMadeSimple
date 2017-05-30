01a1f5661609de48730dd35b1b55c55345d024c6
Tyler Small <intrasolarmusic@gmail.com>
2017-05-30 10:56:24 -0500

    Home controller tests (#92)
    * Refactor existing HomePage test to assert instance type
    
    * Write a test to ensure the mapping of an Issue Entity is called correctly in the controller
    
    * Add explicit test for which View was returned and its existence
    
    * Remove one model from the collection to speed up test run time

--- LobbyingMadeSimple.Tests/Controllers/HomeControllerTest.cs
+++ LobbyingMadeSimple.Tests/Controllers/HomeControllerTest.cs
@@ -5,6 +5,8 @@ using Moq;
 using LobbyingMadeSimple.Core.Interfaces;
 using System.Collections.Generic;
 using LobbyingMadeSimple.Core;
+using LobbyingMadeSimple.Web.Models;
+using DeepEqual.Syntax;
 
 namespace LobbyingMadeSimple.Tests.Controllers
 {
@@ -12,7 +14,24 @@ namespace LobbyingMadeSimple.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void Index_returns_a_view()
+        {
+            // Arrange
+            var repo = new Mock<IIssueRepository>();
+            repo.Setup(r => r.GetTopVotableIssues(15)).Returns(new List<Issue>());
+            repo.Setup(r => r.GetAllFundableIssuesSortedByDate()).Returns(new List<Issue>());
+            HomeController controller = new HomeController(repo.Object);
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("", result.ViewName); // Action uses the default view
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Index_returns_a_homepage_view_model()
         {
             // Arrange
             var repo = new Mock<IIssueRepository>();
@@ -22,9 +41,42 @@ namespace LobbyingMadeSimple.Tests.Controllers
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
+            var model = result.Model;
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(HomePageViewModel));
+        }
+        
+        [TestMethod]
+        public void Index_returns_valid_homepage_view_model_when_results_are_found()
+        {
+            // Arrange
+            var votableIssue = Mock.Of<Issue>(i => i.IsVotableIssue == true && i.GetPrettyPercentage() == "67" && i.Id == 1);
+            var fundableIssue = Mock.Of<Issue>(i => i.IsFundable == true && i.Id == 2);
+            var votableList = new List<Issue>() { votableIssue, votableIssue };
+            var fundableList = new List<Issue>() { fundableIssue, fundableIssue };
+
+            var repo = new Mock<IIssueRepository>();
+            repo.Setup(r => r.GetTopVotableIssues(15)).Returns(votableList);
+            repo.Setup(r => r.GetAllFundableIssuesSortedByDate()).Returns(fundableList);
+            HomeController controller = new HomeController(repo.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            HomePageViewModel model = (HomePageViewModel) result.Model;
+
+            // Assert
+            foreach(VotableHomeIssueViewModel vm in model.VotableIssues)
+            {
+                Assert.AreEqual(votableIssue.GetPrettyPercentage(), vm.ApprovalPercentage);
+                Assert.AreEqual("text-success", vm.ApprovalPercentageColor);
+                Assert.AreEqual(votableIssue.Id, vm.Id);
+            }
+
+            foreach(FundableHomeIssueViewModel vm in model.FundableIssues)
+            {
+                Assert.AreEqual(fundableIssue.Id, vm.Id);
+            }
         }
 
         [TestMethod]
